bf7f026641ad41477cbef5b2b31685f2c3d9652d
sWD4 <shubham2405@gmail.com>
2023-06-17 21:54:39 +0530

    first commit

---
+++ get.js
@@ -0,0 +1,10 @@
+//document.write("Hello ITM")
+
+//let data=document.getElementById("a").innerText;
+//let data=document.getElementById("a").innerHTML;
+// let data=document.getElementById("aa").innerHTML;
+// let data=document.getElementById("a").getAttribute("style");
+//let data=document.getElementById("a").getAttributeNode("style");
+let data=document.getElementById("a").attributes[2];
+
+console.log(data)
